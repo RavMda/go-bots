6841bd23814ac0754fedf6835416b0b1313bf537
Rav <49620461+RavMda@users.noreply.github.com>
2020-12-31 04:59:09 +0400

    reorder config

--- config/config.go
+++ config/config.go
@@ -6,30 +6,31 @@ import (
 )
 
 type Config struct {
-	Host            string `yaml:"host"`
-	Port            string `yaml:"port"`
-	Proxies         string `yaml:"proxy-file"`
-	RegisterCommand string `yaml:"register_command"`
-	LoginCommand    string `yaml:"login_command"`
+	Host     string `yaml:"host"`
+	Port     string `yaml:"port"`
+	Protocol int    `yaml:"protocol"`
 
+	Proxies     string        `yaml:"proxy-file"`
 	Connections int           `yaml:"connections"`
 	Cooldown    time.Duration `yaml:"cooldown"`
 	Timeout     time.Duration `yaml:"timeout"`
-	Protocol    int           `yaml:"protocol"`
 
-	Register   bool `yaml:"register"`
-	DoActivity bool `yaml:"do_activity"`
 	ShouldSpam bool `yaml:"should_spam"`
+	DoActivity bool `yaml:"do_activity"`
 	HitRespond bool `yaml:"hit_respond"`
 
 	PacketSpam     bool          `yaml:"packet_spam"`
 	PacketCooldown time.Duration `yaml:"packet_cooldown"`
 
+	Register        bool   `yaml:"register"`
+	RegisterCommand string `yaml:"register_command"`
+	LoginCommand    string `yaml:"login_command"`
+
+	Phrases []string `yaml:"phrases"`
+
 	Address string
 	Guard   chan struct{}
 	Bots    int
-
-	Phrases []string `yaml:"phrases"`
 }
 
 var (
